3d35b956f5d2393a1b7e15b842f3692fccfd0a0f
James Li <jli11@wayfair.com>
2022-06-06 00:59:00 -0500

    remove wrong name

--- query.go
+++ query.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 	"sort"
 
-	"github.com/shurcooL/graphql/ident"
+	"github.com/jamesyli/graphql/ident"
 )
 
 func constructQuery(v interface{}, variables map[string]interface{}) string {
